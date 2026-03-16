8768826a6d01cbf99db4d54ca8336b4cd22c03b8
Pawel Wludyka <pwludyka@op.pl>
2026-03-16 16:08:06 +0100

    Secrets Management

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from mlops-ipum-lab!")
+import os
+import argparse
+from dotenv import load_dotenv
+from settings import Settings
+import yaml
+
+
+def export_envs(environment: str = "dev") -> None:
+    load_dotenv(f"config/.env.{environment}")
+
+
+def load_yaml():
+    with open("secrets.yaml", "r") as file:
+        secrets = yaml.safe_load(file)
+    API = secrets["API"]
+    os.environ["API"] = API
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description="Load environment variables from specified.env file."
+    )
+    parser.add_argument(
+        "--environment",
+        type=str,
+        default="dev",
+        help="The environment to load (dev, test, prod)",
+    )
+    args = parser.parse_args()
+
+    export_envs(args.environment)
+    load_yaml()
+
+    settings = Settings()
+
+    print("APP_NAME: ", settings.APP_NAME)
+    print("ENVIRONMENT: ", settings.ENVIRONMENT)
+    print("API: ", settings.API)
--- settings.py
+++ settings.py
@@ -6,10 +6,11 @@ from pydantic import field_validator
 class Settings(BaseSettings):
     ENVIRONMENT: str
     APP_NAME: str
+    API: str
 
     @field_validator("ENVIRONMENT")
     @classmethod
     def validate_environment(cls, value):
-        ...  # implement me!
-        # prepare validator that will check whether the value of ENVIRONMENT is in (dev, test, prod)
+        if value not in ("dev", "test", "prod"):
+            raise ValueError("ENVIRONMENT must be one of: dev, test, prod")
         return value
